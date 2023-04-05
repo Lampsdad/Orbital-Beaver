a4a65767b4a08918a5b4d3a939264a71ce68ab67
frankenfurth <60515397+frankenfurth@users.noreply.github.com>
2023-04-05 14:23:11 -0500

    Robot.txt parser update
    Added request fetching and status code checking for the directories listed as disallowed in the robots.txt file

--- RobotsTXTScan.py
+++ RobotsTXTScan.py
@@ -7,7 +7,7 @@ def main():
     #take in a base url from the user
     url = input("Enter the URL to be tested: ")
     #send a request to the website to see if there is a robots.txt file
-    r = requests.get(url + "/robots.txt")
+    r = requests.get(url + "robots.txt")
     #check if the status code is 200
     if r.status_code == 200:
         #print the contents of the robots.txt file
@@ -18,9 +18,18 @@ def main():
         for line in r.text.split("\n"):
             if line.startswith("Disallow:"):
                 disallowed.append(line.split("Disallow:")[1].strip())
-        print(disallowed)
         ##############################################################################
         #Send requests to the website to see if the contents of the robots.txt file are accessible
+        for i in disallowed:
+            #send a request to the website to see if the contents of the robots.txt file are accessible
+            r = requests.get(url + i)
+            #check if the status code is 200
+            if r.status_code == 200:
+                #print that the contents of the directory file are accessible
+                print("HIT: " + i)
+            if r.status_code == 403:
+                #print that the contents of the directory are forbidden
+                print("403: " + i)
     else:
         #print that there is no robots.txt file
         print("There is no robots.txt file")
