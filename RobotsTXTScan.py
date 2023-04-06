cb29950cdb3d03e71fe77c48da4a1f59c75a1b25
frankenfurth <60515397+frankenfurth@users.noreply.github.com>
2023-04-05 19:20:18 -0500

    Update RobotsTXTScan.py

--- RobotsTXTScan.py
+++ RobotsTXTScan.py
@@ -8,17 +8,26 @@ def main():
     url = input("Enter the URL to be tested: ")
     #send a request to the website to see if there is a robots.txt file
     r = requests.get(url + "robots.txt")
+
+
     #check if the status code is 200
     if r.status_code == 200:
-        #print the contents of the robots.txt file
-        print(r.text)
-        ###################COULD PROBABLY BE OPTIMIZED?###############################
+        ###################COULD PROBABLY BE REGEX?###################################
         #split the URL contents of r.text into a list
         disallowed = []
         for line in r.text.split("\n"):
             if line.startswith("Disallow:"):
                 disallowed.append(line.split("Disallow:")[1].strip())
         ##############################################################################
+
+
+        #Remove duplicates from the diasllowed list
+        disallowed = list(set(disallowed))
+        #Remove any empty strings from the list
+        disallowed = list(filter(None, disallowed))
+        #Remove any '/' from the list
+        disallowed = [x for x in disallowed if x != '/']
+
         #Send requests to the website to see if the contents of the robots.txt file are accessible
         for i in disallowed:
             #send a request to the website to see if the contents of the robots.txt file are accessible
